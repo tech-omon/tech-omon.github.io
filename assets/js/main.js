b140e92f1e11af69a0d4fe0a9e9909a303768ba9
Shubham Bisht <shubham99bisht@gmail.com>
2021-02-18 14:46:56 +0530

    First look

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,20 +32,3 @@ if ($('.nav-menu').length) {
 } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
   $(".mobile-nav, .mobile-nav-toggle").hide();
 }
-
-
-// Toggle .header-scrolled class to #header when page is scrolled
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 100) {
-    $('#header').addClass('header-scrolled');
-    $('#topbar').addClass('topbar-scrolled');
-  } else {
-    $('#header').removeClass('header-scrolled');
-    $('#topbar').removeClass('topbar-scrolled');
-  }
-});
-
-if ($(window).scrollTop() > 100) {
-  $('#header').addClass('header-scrolled');
-  $('#topbar').addClass('topbar-scrolled');
-}
